0e6bc157910ea415ae31e2c51690e3ec7e6d7855
Indran Jayatharan <indran@calcey.com>
2022-04-15 20:37:45 +0530

    react bootstrap

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,9 +1,12 @@
 import React from 'react';
+import '../theme/defaultTheme.scss';
+import Button from '../shared/Button';
 
 function App() {
   return (
     <div className="App">
         <h1>Application Begin</h1>
+        <Button>Hello World</Button>
     </div>
   );
 }
---
+++ src/shared/Button.tsx
@@ -0,0 +1,6 @@
+import styled from 'styled-components';
+import { Button as BootstrapButton } from 'react-bootstrap';
+
+const Button = styled(BootstrapButton)``;
+
+export default Button;
