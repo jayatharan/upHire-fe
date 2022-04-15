422a55d6fce4653594a2d3070fd9123999933cea
Indran Jayatharan <indran@calcey.com>
2022-04-15 20:02:37 +0530

    AuthService

--- src/apis/AxiosInstance.ts
+++ src/apis/AxiosInstance.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const axiosInstance = axios.create({
     baseURL: process.env.REACT_APP_API_BASE_URL
- });
+});
 
 export const setAuthHeader = (authToken: string | undefined) => {
     if (authToken) {
